326e52bb73e6b027d676a1ac34b10776a8925dfe
Elli610 <113121626+Elli610@users.noreply.github.com>
2023-04-14 10:26:11 +0200

    add src/account/Account.ts documentation

--- src/account/Account.ts
+++ src/account/Account.ts
@@ -1,3 +1,12 @@
+/**
+ * This module contains the Account class. It is responsible for representing an account in the wallet. 
+ * 
+ * @remarks
+ * This class provides methods to interact with the account's {@link balance} and to {@link sign} messages.
+ *
+ * @module
+ */
+
 import {
   IAccountBalanceRequest,
   IAccountBalanceResponse,
@@ -12,24 +21,50 @@ export class Account {
   private _address: string;
   private _name: string;
 
+  /**
+   * @constructor
+   * @param address - The address of the account.
+   * @param name - The name of the account.
+   * @param providerName - The name of the provider.
+   * @returns An instance of the Account class.
+   * 
+   * @remarks
+   * - The Account constructor takes an object of type IAccount and a providerName string as its arguments. 
+   * - The IAccount object contains the account's address and name. 
+   * - The providerName string identifies the provider that is used to interact with the blockchain.
+   */
   public constructor({ address, name }: IAccount, providerName: string) {
     this._address = address;
     this._name = name;
     this._providerName = providerName;
   }
 
+  /**
+   * @returns The address of the account.
+   */
   public address(): string {
     return this._address;
   }
 
+  /**
+   * @returns The name of the account.
+   */
   public name(): string {
     return this._name;
   }
 
+  /**
+   * @returns The name of the provider.
+   */
   public providerName(): string {
     return this._providerName;
   }
 
+  /**
+   * This method aims to retrieve the account's balance.
+   * 
+   * @returns A promise that resolves to an object of type IAccountBalanceResponse. It contains the account's balance.
+   */
   public async balance(): Promise<IAccountBalanceResponse> {
     return new Promise<IAccountBalanceResponse>((resolve, reject) => {
       connector.sendMessageToContentScript(
@@ -44,6 +79,12 @@ export class Account {
     });
   }
 
+  /**
+   * This method aims to sign a message.
+   * 
+   * @param data - The message to be signed.
+   * @returns A promise that resolves to an object of type IAccountSignResponse. It contains the signature of the message.
+   */
   public async sign(data: Uint8Array): Promise<IAccountSignResponse> {
     return new Promise<IAccountSignResponse>((resolve, reject) => {
       connector.sendMessageToContentScript(
