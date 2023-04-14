436a22c9c799f47e4976875d2beb486293f17c08
Elli610 <113121626+Elli610@users.noreply.github.com>
2023-04-14 13:30:05 +0200

    Update src/account/account.ts

--- src/account/Account.ts
+++ src/account/Account.ts
@@ -1,11 +1,3 @@
-/**
- * @dependencies
- * - IAccountBalanceRequest and IAccountBalanceResponse from the ./AccountBalance module
- * - IAccountSignRequest and IAccountSignResponse from the ./AccountSign module
- * - connector from the ../connector/Connector module
- * - IAccount from the ./IAccount module
- * - AvailableCommands from ../index.ts
- */
 import {
   IAccountBalanceRequest,
   IAccountBalanceResponse,
@@ -16,17 +8,16 @@ import { IAccount } from './IAccount';
 import { AvailableCommands } from '..';
 
 /**
- * This module contains the Account class. It is responsible for representing an account in the wallet. 
- * 
+ * This module contains the Account class. It is responsible for representing an account in the wallet.
+ *
  * @remarks
  * - This class provides methods to interact with the account's {@link balance} and to {@link sign} messages.
- * 
- * @properties
- * - _providerName - The name of the provider -> private
- * - _address - The address of the account -> private
- * - _name - The name of the account -> private
- * 
- * @module
+ *
+ * Properties:
+ * - _providerName - The name of the provider - private
+ * - _address - The address of the account - private
+ * - _name - The name of the account - private
+ *
  */
 export class Account {
   private _providerName: string;
@@ -34,15 +25,16 @@ export class Account {
   private _name: string;
 
   /**
-   * @constructor
+   * This constructor takes an object of type IAccount and a providerName string as its arguments.
+   *
    * @param address - The address of the account.
    * @param name - The name of the account.
    * @param providerName - The name of the provider.
    * @returns An instance of the Account class.
-   * 
+   *
    * @remarks
-   * - The Account constructor takes an object of type IAccount and a providerName string as its arguments. 
-   * - The IAccount object contains the account's address and name. 
+   * - The Account constructor takes an object of type IAccount and a providerName string as its arguments.
+   * - The IAccount object contains the account's address and name.
    * - The providerName string identifies the provider that is used to interact with the blockchain.
    */
   public constructor({ address, name }: IAccount, providerName: string) {
@@ -74,7 +66,7 @@ export class Account {
 
   /**
    * This method aims to retrieve the account's balance.
-   * 
+   *
    * @returns A promise that resolves to an object of type IAccountBalanceResponse. It contains the account's balance.
    */
   public async balance(): Promise<IAccountBalanceResponse> {
@@ -93,9 +85,9 @@ export class Account {
 
   /**
    * This method aims to sign a message.
-   * 
+   *
    * @param data - The message to be signed.
-   * @returns A promise that resolves to an object of type IAccountSignResponse. It contains the signature of the message.
+   * @returns An IAccountSignResponse object. It contains the signature of the message.
    */
   public async sign(data: Uint8Array): Promise<IAccountSignResponse> {
     return new Promise<IAccountSignResponse>((resolve, reject) => {
