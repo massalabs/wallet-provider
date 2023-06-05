13b3ba675e8574e6a7661055ea2dbdb7c0fbe8e5
Elli610 <113121626+Elli610@users.noreply.github.com>
2023-06-05 10:31:02 +0200

    add documentation

--- src/account/IDryRunData.ts
+++ src/account/IDryRunData.ts
@@ -1,3 +1,9 @@
+/**
+ * This interface defines specific data that are used to perform a dry run.
+ *
+ * @see dryRun - Wether or not to perform a dry run
+ * @see maxGas - The maximum amount of gas to be used in the dry run
+ */
 export interface IDryRunData {
   dryRun: boolean;
   maxGas?: bigint;
