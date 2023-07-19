0290cba034b48f2e79b2107da8ab9e219ac906f3
Nathan HERVIER <113121626+Elli610@users.noreply.github.com>
2023-07-19 15:56:07 +0200

    simplify the init of provider (#77)
    Co-authored-by: Grégory Libert <gl@massa.net>

--- src/connector/index.ts
+++ src/connector/index.ts
@@ -1,4 +1,9 @@
-export { connector, AllowedRequests, AllowedResponses } from './Connector';
+export {
+  connector,
+  AllowedRequests,
+  AllowedResponses,
+  MASSA_WINDOW_OBJECT,
+} from './Connector';
 export { ICustomEventMessageRequest } from './ICustomEventMessageRequest';
 export { ICustomEventMessageResponse } from './ICustomEventMessageResponse';
 export { IRegisterEvent } from './IRegisterEvent';
--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ if (typeof window !== 'undefined') {
   window.Buffer = Buffer;
 }
 
-import { connector } from './connector/Connector';
+import { connector, MASSA_WINDOW_OBJECT } from './connector/Connector';
 import { IProvider } from './provider/IProvider';
 import { Provider } from './provider/Provider';
 import {
@@ -38,18 +38,52 @@ export interface ITransactionDetails {
   operationId: string;
 }
 
-export function providers(): IProvider[] {
-  let providers: IProvider[] = [];
-  for (const providerName of Object.keys(connector.getWalletProviders())) {
-    if (providerName === MASSA_STATION_PROVIDER_NAME) {
-      const p = new MassaStationProvider();
-      providers.push(p);
-    } else {
-      const p = new Provider(providerName);
-      providers.push(p);
+/**
+ * Get the list of providers that are available to interact with.
+ *
+ * @param retry - If true, will retry to get the list of providers if none are available.
+ * @param timeout - The timeout in milliseconds to wait between retries. default is 2000ms.
+ *
+ * @returns An array of providers.
+ */
+export async function providers(
+  retry = true,
+  timeout = 500,
+): Promise<IProvider[]> {
+  let provider: IProvider[] = [];
+  while (provider.length === 0) {
+    for (const providerName of Object.keys(connector.getWalletProviders())) {
+      if (providerName === MASSA_STATION_PROVIDER_NAME) {
+        const p = new MassaStationProvider();
+        provider.push(p);
+      } else {
+        const p = new Provider(providerName);
+        provider.push(p);
+      }
+    }
+    // If no providers are available, wait and try again
+    if (retry && provider.length === 0) {
+      await new Promise((resolve) => setTimeout(resolve, timeout));
     }
   }
-  return providers;
+
+  return provider;
+}
+
+/**
+ * Manually register a provider to interact with.
+ *
+ * @param name - The name of the provider.
+ * @param id - The id of the HTML element that is used to communicate with the provider.
+ */
+export function registerProvider(name: string, id = MASSA_WINDOW_OBJECT): void {
+  const registerEvent = new CustomEvent('register', {
+    detail: { providerName: name },
+  });
+  const element = document.getElementById(id);
+  if (element) {
+    element.dispatchEvent(registerEvent);
+  }
 }
 
 export { AllowedRequests, AllowedResponses } from './connector';
