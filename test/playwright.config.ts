d2f38c71081b75d47efd26d82c8a7d042f55a61d
Ben <br@massa.net>
2023-09-02 09:15:36 +0200

    Test disable firefox

--- test/playwright.config.ts
+++ test/playwright.config.ts
@@ -36,11 +36,10 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
   ],
 
   /* Run your local dev server before starting the tests */
