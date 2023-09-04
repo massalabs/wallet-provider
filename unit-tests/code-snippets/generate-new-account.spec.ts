8392b911e651336a54144f50807c0913a73eaa91
Grégory Libert <gl@massa.net>
2023-09-04 15:29:12 +0200

    Adds code snippet generation

--- unit-tests/code-snippets/generate-new-account.spec.ts
+++ unit-tests/code-snippets/generate-new-account.spec.ts
@@ -2,17 +2,15 @@ import { providers } from '@massalabs/wallet-provider';
 
 it('should generate a new account with name and address', async () => {
   const availableProviders = await providers();
-  console.log(
-    'Available providers:',
-    availableProviders.map((p) => p.name()),
-  );
   const massaStationProvider = availableProviders.find(
     (p) => p.name() === 'MASSASTATION',
   );
 
+  // stop the test if the provider is not available
   if (!massaStationProvider)
     throw new Error('Massa Station provider not found');
 
+  // generate a new account
   const newAccount = await massaStationProvider.generateNewAccount(
     'my-massa-wallet',
   );
@@ -20,6 +18,7 @@ it('should generate a new account with name and address', async () => {
   expect(newAccount).toHaveProperty('name');
   expect(newAccount).toHaveProperty('address');
 
+  // print the account name and address
   console.log(
     'Account Name:',
     newAccount.name || 'no name',
