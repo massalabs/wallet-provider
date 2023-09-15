49db6ae054ab6bcd46590caf62d7a650cb94e242
Grégory Libert <gl@massa.net>
2023-09-15 16:26:09 +0200

    Format

--- unit-tests/code-snippets/operation-buy-sell-roll.spec.ts
+++ unit-tests/code-snippets/operation-buy-sell-roll.spec.ts
@@ -1,30 +1,39 @@
-import { providers } from "@massalabs/wallet-provider";
-import { ClientFactory, fromMAS } from "@massalabs/massa-web3";
+import { providers } from '@massalabs/wallet-provider';
+import { ClientFactory, fromMAS } from '@massalabs/massa-web3';
 
 it('should create new operations: buy and sell roll', async () => {
   const availableProviders = await providers();
   const massaStationProvider = availableProviders.find(
     (p) => p.name() === 'MASSASTATION',
   );
-  
+
   // stop the test if the provider is not available
   if (!massaStationProvider)
     throw new Error('Massa Station provider not found');
 
   // initialize the web3 client
   const firstAccount = (await massaStationProvider.accounts())[0];
-  const web3client = await ClientFactory.fromWalletProvider(massaStationProvider, firstAccount);
+  const web3client = await ClientFactory.fromWalletProvider(
+    massaStationProvider,
+    firstAccount,
+  );
 
   // create a new buy and sell roll operations
-  const buyOperationId = await web3client.wallet().buyRolls({ fee: 0n, amount: 1n });
-  const sellOperationId = await web3client.wallet().sellRolls({ fee: 0n, amount: 1n });
+  const buyOperationId = await web3client
+    .wallet()
+    .buyRolls({ fee: 0n, amount: 1n });
+  const sellOperationId = await web3client
+    .wallet()
+    .sellRolls({ fee: 0n, amount: 1n });
 
   expect(buyOperationId).toBeDefined();
   expect(sellOperationId).toBeDefined();
-  
+
   // print the operation ids
   console.log(
-    "Buy roll operation id", buyOperationId,
-		"Sell roll operation id", sellOperationId
+    'Buy roll operation id',
+    buyOperationId,
+    'Sell roll operation id',
+    sellOperationId,
   );
 });
--- unit-tests/code-snippets/operation-call-sc.spec.ts
+++ unit-tests/code-snippets/operation-call-sc.spec.ts
@@ -1,11 +1,11 @@
-import { providers } from "@massalabs/wallet-provider";
-import { ClientFactory } from "@massalabs/massa-web3";
-import { C8ex2d7X8pkBlockchainCaller } from "../../helpers/8ex2d7X8pkCaller";
+import { providers } from '@massalabs/wallet-provider';
+import { ClientFactory } from '@massalabs/massa-web3';
+import { C8ex2d7X8pkBlockchainCaller } from '../../helpers/8ex2d7X8pkCaller';
 
 // Before running this test, you need to:
 // - deploy the smart contract `hello world` to the blockchain.
 // - replace the address below with the address of the deployed smart contract.
-// - generate the smart contract helper executing the following command: 
+// - generate the smart contract helper executing the following command:
 //   `npx massa-proto --addr=AS12CvQGUaaSd933VNQGoQ8zja2Xvs6GhRJMDswLq2p8ex2d7X8pk --gen=web3`
 
 it('should create a new operation: call SC', async () => {
@@ -13,18 +13,23 @@ it('should create a new operation: call SC', async () => {
   const massaStationProvider = availableProviders.find(
     (p) => p.name() === 'MASSASTATION',
   );
-  
+
   // stop the test if the provider is not available
   if (!massaStationProvider)
     throw new Error('Massa Station provider not found');
 
   // initialize the web3 client
   const firstAccount = (await massaStationProvider.accounts())[0];
-  const web3client = await ClientFactory.fromWalletProvider(massaStationProvider, firstAccount);
+  const web3client = await ClientFactory.fromWalletProvider(
+    massaStationProvider,
+    firstAccount,
+  );
 
   // call the smart contract
-  const caller = await C8ex2d7X8pkBlockchainCaller.newDefault(web3client.wallet());
-  const result = await caller.helloWorld("Hey there!");
+  const caller = await C8ex2d7X8pkBlockchainCaller.newDefault(
+    web3client.wallet(),
+  );
+  const result = await caller.helloWorld('Hey there!');
 
   console.log(result);
 });
