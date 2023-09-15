5647bc4308e4dd47d6e80510be7e18033d6dc14f
Ben <br@massa.net>
2023-09-15 09:08:34 +0200

    Add code snippet for getNetwork

--- code-snippets/get-network.ts
+++ code-snippets/get-network.ts
@@ -1,17 +1,18 @@
 import { providers } from '@massalabs/wallet-provider';
 
-it('should get network', async () => {
+(async () => {
   const availableProviders = await providers();
   const massaStationProvider = availableProviders.find(
     (p) => p.name() === 'MASSASTATION',
   );
 
+  // stop the test if the provider is not available
   if (!massaStationProvider)
     throw new Error('Massa Station provider not found');
 
+  // Get current network
   const network = await massaStationProvider.getNetwork();
 
-  expect(['mainnet', 'testnet', 'buildnet', 'labnet']).toContain(network);
-
+  // Print the network name
   console.log('Network Name:', network);
-});
+})();
