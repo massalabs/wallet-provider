b2c48676eca0b3b2e07b65ad83031d4845560cbe
Ben <br@massa.net>
2023-09-28 21:00:07 +0200

    Remove the property name from the documentation of the AccountBalance

--- src/account/AccountBalance.ts
+++ src/account/AccountBalance.ts
@@ -1,7 +1,7 @@
 /**
  * Represents the request for an AccountBalance command.
  *
- * @property address - The account address.
+ * address - The account address.
  */
 export interface IAccountBalanceRequest {
   address: string;
@@ -10,8 +10,8 @@ export interface IAccountBalanceRequest {
 /**
  * Represents the response from an AccountBalance command.
  *
- * @property finalBalance - The account's final confirmed balance.
- * @property candidateBalance - The account's pending balance.
+ * finalBalance - The account's final confirmed balance.
+ * candidateBalance - The account's pending balance.
  */
 export interface IAccountBalanceResponse {
   finalBalance: string;
