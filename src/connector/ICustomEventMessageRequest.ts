f460b14475b2c9086289a4a6e74a4619faa4436f
Ben <br@massa.net>
2023-10-02 09:23:21 +0200

    Improve or add documentation for src/connector folder (#174)

--- src/connector/ICustomEventMessageRequest.ts
+++ src/connector/ICustomEventMessageRequest.ts
@@ -1,10 +1,17 @@
 import { AllowedRequests } from './Connector';
 
 /**
- * This interface represents the request done by the web page script to the content script
- * in order to setup a custom event.
+ * Represents a request sent from the web page script to the content script
+ * to initiate a custom event.
+ *
+ * @remarks
+ * The request is used for setting up communication for a specific operation,
+ * identified by a unique `requestId`.
  */
 export interface ICustomEventMessageRequest {
+  /** Parameters for the request, conforming to allowed request types. */
   params: AllowedRequests;
+
+  /** Unique identifier for the request. */
   requestId: string;
 }
--- src/connector/ICustomEventMessageResponse.ts
+++ src/connector/ICustomEventMessageResponse.ts
@@ -1,11 +1,16 @@
 import { AllowedResponses } from './Connector';
 
 /**
- * This interface represents the response from the content script to the web page script
- * for an ICustomEventMessageRequest.
+ * Represents the response from the content script to the web page script
+ * following an `ICustomEventMessageRequest`.
  */
 export interface ICustomEventMessageResponse {
+  /** The result of the request, defined by `AllowedResponses` type. */
   result?: AllowedResponses;
+
+  /** Any error that occurred during the request processing. */
   error?: Error;
+
+  /** The unique identifier of the request. */
   requestId: string;
 }
