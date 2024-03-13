778adaf6b0a83e8e854fb539af32aeff268211b9
BenRey <br@massa.net>
2024-03-13 11:17:34 +0100

    Fix test missing account

--- test-e2e/code-snippets/account-get-balance.spec.ts
+++ test-e2e/code-snippets/account-get-balance.spec.ts
@@ -1,11 +1,19 @@
 import { providers } from '@massalabs/wallet-provider';
+import { deleteStationAccountFromNickname } from '../utils/provider-utils';
 
-it('should get network url', async () => {
+afterAll(async () => {
+  await deleteStationAccountFromNickname('signing-account');
+});
+
+it('should get the balance', async () => {
   const availableProviders = await providers();
   const massaStationProvider = availableProviders.find(
     (p) => p.name() === 'MASSASTATION',
   );
 
+  const accountName = 'my-account';
+
+  await massaStationProvider.generateNewAccount(accountName);
   if (!massaStationProvider)
     throw new Error('Massa Station provider not found');
 
