c72d9d2bc16c72c8597f223748e6fb58a76aa334
BenRey <br@massa.net>
2024-03-13 11:17:34 +0100

    Fix test missing account

--- test-e2e/code-snippets/account-get-address.spec.ts
+++ test-e2e/code-snippets/account-get-address.spec.ts
@@ -1,10 +1,18 @@
 import { providers } from '@massalabs/wallet-provider';
+import { deleteStationAccountFromNickname } from '../utils/provider-utils';
+
+afterAll(async () => {
+  await deleteStationAccountFromNickname('signing-account');
+});
 
 it('should get network url', async () => {
   const availableProviders = await providers();
   const massaStationProvider = availableProviders.find(
     (p) => p.name() === 'MASSASTATION',
   );
+
+  const accountName = 'my-account';
+  await massaStationProvider.generateNewAccount(accountName);
 
   if (!massaStationProvider)
     throw new Error('Massa Station provider not found');
--- test-e2e/code-snippets/account-sign.spec.ts
+++ test-e2e/code-snippets/account-sign.spec.ts
@@ -1,22 +1,45 @@
 import { providers } from '@massalabs/wallet-provider';
+import { deleteStationAccountFromNickname } from '../utils/provider-utils';
 
-it('should get network url', async () => {
-  const availableProviders = await providers();
-  const massaStationProvider = availableProviders.find(
-    (p) => p.name() === 'MASSASTATION',
-  );
+describe('Sign a message', () => {
+  afterAll(async () => {
+    await deleteStationAccountFromNickname('signing-account');
+  });
 
-  if (!massaStationProvider)
-    throw new Error('Massa Station provider not found');
+  it('should generate a new account and sign data with it', async () => {
+    const availableProviders = await providers();
+    const massaStationProvider = availableProviders.find(
+      (p) => p.name() === 'MASSASTATION',
+    );
 
-  const accounts = await massaStationProvider.accounts();
-  const selectedAccount = accounts[0];
+    // stop the test if the provider is not available
+    if (!massaStationProvider)
+      throw new Error('Massa Station provider not found');
 
-  const message = 'hello world';
-  const signature = await selectedAccount.sign(message);
+    const accountName = 'signing-account';
+    const dataToSign = 'test';
 
-  expect(signature).toHaveProperty('publicKey');
-  expect(signature).toHaveProperty('base58Encoded');
+    // generate a new account
+    await massaStationProvider.generateNewAccount(accountName);
 
-  console.log('Signature:', signature);
+    // get account object
+    const accounts = await massaStationProvider.accounts();
+    const newAccount = accounts.find((a) => a.name() === accountName);
+
+    const resp = await newAccount.sign(dataToSign);
+
+    expect(resp).not.toBeNull();
+    expect(resp.publicKey).not.toBeNull();
+    expect(resp.base58Encoded).not.toBeNull();
+
+    // print the account name and address
+    console.log(
+      'Account Address:',
+      newAccount.address,
+      'signed data:',
+      dataToSign,
+      'signature:',
+      resp.base58Encoded,
+    );
+  });
 });
