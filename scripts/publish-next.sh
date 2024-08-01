a679a44aa18dd8bbc32d8586abf20d945430a1fa
Pierre Seznec <petterseznec@gmail.com>
2024-08-01 10:43:10 +0200

    fix npm publish

--- scripts/publish-next.sh
+++ scripts/publish-next.sh
@@ -14,5 +14,9 @@ npm version --preid $TAG --no-git-tag-version --no-commit-hooks premajor
 TIME=$(date -u +%Y%m%d%H%M%S)
 sed -i "/version/s/$TAG.0/$TAG.$TIME/g" package.json
 
+PACKAGE_NAME=$(cat package.json | jq -r '.name')
+PUBLISH_VERSION=$(cat package.json | jq -r '.version')
+echo publishing ${PACKAGE_NAME}@$PUBLISH_VERSION
+
 # Publish the package
 npm publish --access public --tag $TAG
