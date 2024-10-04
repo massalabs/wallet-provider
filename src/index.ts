0c7c1d0f31ced90cc91ad92e9b602318853f8d7c
Pierre Seznec <petterseznec@gmail.com>
2024-10-04 11:48:12 +0200

    remove unused dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,9 @@
 /** Polyfills */
-import { Buffer } from 'buffer';
 
 declare global {
   interface Window {
-    Buffer: typeof Buffer;
     bearby?: unknown;
   }
-}
-
-// Check if we are on browser
-if (typeof window !== 'undefined') {
-  window.Buffer = Buffer;
 }
 
 export enum AvailableCommands {
