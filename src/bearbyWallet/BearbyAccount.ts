6cd896e70436b1dceee08c745a3386bb3cda05c4
Phoebe Lartisant <phoebelartisant@proton.me>
2024-11-21 16:10:33 -0500

    add gasPrice

--- src/bearbyWallet/BearbyAccount.ts
+++ src/bearbyWallet/BearbyAccount.ts
@@ -257,6 +257,7 @@ export class BearbyAccount implements Provider {
       maxGas: params.maxGas || MAX_GAS_DEPLOYMENT,
       coins: params.coins,
       fee: fee,
+      gasPrice: 10000n, // dummy value waiting for (https://github.com/bearby-wallet/bearby-web3/pull/25)
       contractDataBase64: uint8ArrayToBase64(params.byteCode),
       deployerBase64: uint8ArrayToBase64(DEPLOYER_BYTECODE),
     };
