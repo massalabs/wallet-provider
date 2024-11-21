ebea25957592eecf1c4b40c283475ab97cbadca9
BenRey <br@massa.net>
2024-11-21 10:47:38 +0100

    update massa-web3 version and implement executeSC method in BearbyAccount and MassaStationAccount classes

--- src/massaStation/MassaStationAccount.ts
+++ src/massaStation/MassaStationAccount.ts
@@ -238,6 +238,10 @@ export class MassaStationAccount implements Provider {
     throw new Error('Method not implemented.');
   }
 
+  executeSC(): Promise<Operation> {
+    throw new Error('Method not implemented.');
+  }
+
   public async getOperationStatus(opId: string): Promise<OperationStatus> {
     const client = await getClient();
     // This implementation is wrong. We should use massaStation instead of targeting the node directly.
