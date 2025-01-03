4399b622039e7fa201f9fefb2403d7fdad42143d
Phoebe Lartisant <phoebelartisant@proton.me>
2025-01-03 12:30:00 -0500

    Add install links to wallets (#284)
    * add install links to wallets
    
    * Revert "add install links to wallets"
    
    This reverts commit 627bd307a7ef6ba165e38039cb3e83783f953771.
    
    * export wallet install links

---
+++ src/wallet/constants.ts
@@ -0,0 +1,7 @@
+import { WalletName } from './types';
+
+export const WalletInstallLink: Record<WalletName, string> = {
+  [WalletName.Bearby]: 'https://bearby.io',
+  [WalletName.MassaWallet]: 'https://station.massa.net/',
+  [WalletName.Metamask]: 'https://metamask.io/download/',
+};
