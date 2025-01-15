908943010e86444db0b64acb0dd79517d11edd3a
phoebe <phoebe.lartisant@proton.me>
2025-01-15 09:26:34 -0500

    change wallet install link

--- src/wallet/constants.ts
+++ src/wallet/constants.ts
@@ -3,5 +3,6 @@ import { WalletName } from './types';
 export const WalletInstallLink: Record<WalletName, string> = {
   [WalletName.Bearby]: 'https://bearby.io',
   [WalletName.MassaWallet]: 'https://station.massa.net/',
-  [WalletName.Metamask]: 'https://metamask.io/download/',
+  [WalletName.Metamask]:
+    'https://snaps.metamask.io/snap/npm/massalabs/metamask-snap/',
 };
