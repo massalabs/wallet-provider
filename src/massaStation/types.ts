10e851d68d56d84590ad39ba45d8b5cea4e5cbb7
Peterjah <ps@massa.net>
2025-02-11 13:07:20 +0100

    add MS Wallet signRule management

--- src/massaStation/types.ts
+++ src/massaStation/types.ts
@@ -96,3 +96,30 @@ export type MSAccountSignResp = {
 export type MSSendOperationResp = {
   operationId: string;
 };
+
+export interface AccountConfig {
+  signRules: SignRule[];
+}
+
+export interface Config {
+  accounts: {
+    [key: string]: AccountConfig;
+  };
+}
+
+export enum RuleType {
+  DisablePasswordPrompt = 'DISABLE_PASSWORD_PROMPT',
+  AutoSign = 'AUTO_SIGN',
+}
+
+export interface SignRule {
+  id?: string;
+  ruleType: RuleType;
+  name?: string;
+  contract: string;
+  enabled: boolean;
+}
+
+export type AddUpdateSignRuleResponse = {
+  id: string;
+};
