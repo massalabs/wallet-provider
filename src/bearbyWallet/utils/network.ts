38e3a360a524a715f54441ebc05f0425bebbeae4
Peterjah <ps@massa.net>
2025-02-18 09:33:34 +0100

    update bearby-web3

--- src/bearbyWallet/utils/network.ts
+++ src/bearbyWallet/utils/network.ts
@@ -3,9 +3,8 @@ import { Mas, Network } from '@massalabs/massa-web3';
 
 export const networkInfos = async (): Promise<Network> => {
   const { net } = await web3.wallet.network;
-  // TODO: fix bearby.js typings
-  const res = (await web3.massa.getNodesStatus()) as any;
-  if (res.error) {
+  const res = await web3.massa.getNodesStatus();
+  if (res.error || !res.result) {
     throw new Error(res.error?.message || 'Bearby getNodesStatus error');
   }
   return {
