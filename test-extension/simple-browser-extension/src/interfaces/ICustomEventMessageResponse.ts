d977fde1c45c1801c90f96b611d0bd04377d1ef3
Peterjah <ps@massa.net>
2025-05-09 12:49:41 +0200

    remove legacy extension tests

--- test-extension/simple-browser-extension/src/interfaces/ICustomEventMessageResponse.ts
+++
@@ -1,5 +0,0 @@
-export interface ICustomEventMessageResponse {
-  result?: any;
-  error?: Error;
-  requestId: string;
-}
--- test-extension/simple-browser-extension/src/interfaces/IRollOperations.ts
+++
@@ -1,4 +0,0 @@
-export interface IRollOperations {
-  amount: string;
-  fee: string;
-}
